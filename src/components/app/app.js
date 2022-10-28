9e79cf858aa56c09227025c75c4ade6571ba4c23
George <kositskiy2003@mail.ru>
2022-10-28 19:44:59 +0300

    react lazy optimization

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,20 +1,17 @@
-import { Component } from "react";
-
+import { Component, lazy, Suspense } from "react";
 import { Routes, Route, useParams } from "react-router-dom";
-
-import CoffeeHouse from "../coffee-house/coffee-house";
-import OurCoffee from "../our-coffee/our-coffee";
-import Pleasure from "../pleasure/pleasure";
-import Divider from "../divider/divider";
-import CoffeeItemAbout from "../coffee-item-about/coffee-item-about";
-
-
 
 import aromistico from "../../assets/img/aromistico.png";
 import solimo from "../../assets/img/solimo.png";
 import presto from "../../assets/img/presto.png";
 
 import "./app.scss";
+
+const CoffeeHouse = lazy(() => import('../coffee-house/coffee-house'));
+const OurCoffee = lazy(() => import('../our-coffee/our-coffee'));
+const Pleasure = lazy(() => import('../pleasure/pleasure'));
+const Divider = lazy(() => import('../divider/divider'));
+const CoffeeItemAbout = lazy(() => import('../coffee-item-about/coffee-item-about'));
 
 const data = [
   {
@@ -132,38 +129,41 @@ class App extends Component {
         {/* <CoffeeHouse bestData={best} /> */}
         {/* <OurCoffee changeRegion={this.changeRegion} filterPanel={this.filterPanel} onFilter={this.onFilter} changePage={this.changePage} data={visibleData} filter={filter}/>; */}
         {/* <Pleasure changeRegion={this.changeRegion} filterPanel={this.filterPanel} onFilter={this.onFilter} changePage={this.changePage} data={visibleData} filter={filter}/>; */}
-        <Routes>
-          <Route path="/" element={<CoffeeHouse bestData={best} />} />
-          <Route
-            path="/our-coffee"
-            element={
-              <OurCoffee
-                changeRegion={this.changeRegion}
-                filterPanel={this.filterPanel}
-                onFilter={this.onFilter}
-                data={visibleData}
-                filter={filter}
-              />
-            }
-          />
-          <Route
-            path="/pleasure"
-            element={
-              <Pleasure
-                changeRegion={this.changeRegion}
-                filterPanel={this.filterPanel}
-                onFilter={this.onFilter}
-                data={visibleData}
-                filter={filter}
-              />
-            }
-          />
-          <Route
-          path="/coffee-item-about/:id"
-          element={<CoffeePage/>}
-          />
-        </Routes>
-        <Divider/>
+        <Suspense fallback={<span>Loading...</span>}>
+          <Routes>
+            <Route path="/" element={<CoffeeHouse bestData={best} />} />
+            <Route
+              path="/our-coffee"
+              element={
+                <OurCoffee
+                  changeRegion={this.changeRegion}
+                  filterPanel={this.filterPanel}
+                  onFilter={this.onFilter}
+                  data={visibleData}
+                  filter={filter}
+                />
+              }
+            />
+            <Route
+              path="/pleasure"
+              element={
+                <Pleasure
+                  changeRegion={this.changeRegion}
+                  filterPanel={this.filterPanel}
+                  onFilter={this.onFilter}
+                  data={visibleData}
+                  filter={filter}
+                />
+              }
+            />
+            <Route
+            path="/coffee-item-about/:id"
+            element={<CoffeePage/>}
+            />
+          </Routes>
+          <Divider/>
+        </Suspense>
+        
       </>
     );
 
@@ -183,7 +183,7 @@ class App extends Component {
 function CoffeePage() {
   const params = useParams(); // хук из реакт дома
   const filteredData = data.find(item => item.id === parseInt(params.id)); // поиск элемента массива с таким же id, как и в переменной params
-  return <CoffeeItemAbout id={params.id} data={filteredData}/>
+  return <CoffeeItemAbout data={filteredData}/>
 }
 
 export default App;
